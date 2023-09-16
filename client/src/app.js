0c483da73e54c5ab9062c3631b0c95f029d83575
coderunner86 <java@unicauca.edu.com>
2023-09-15 19:53:57 -0500

    feat: Agregada funcionalidad para crear reserva y actualizar libro desde el cliente

--- client/src/app.js
+++ client/src/app.js
@@ -10,3 +10,46 @@ axios.get(apiUrl)
     console.error('Error al obtener los datos:', error);
   });
 
+const crearReserva = async (idLibro, nombreUsuario) => {
+  try {
+    const response = await axios.post(`http://localhost:3001/bookRequest/${idLibro}/reserva`, {
+      nombreUsuario
+    });
+
+    console.log('Reserva realizada:', response.data.reserva);
+  } catch (error) {
+    console.error('Error al hacer la reserva:', error);
+  }
+};
+
+// Actualizar un libro por su ID (ignorando la reserva si existe)
+const actualizarLibro = async (id, nuevoTitulo, nuevoAutor) => {
+  try {
+    // Obtener la lista de libros
+    const response = await axios.get(apiUrl);
+
+    // Encuentra el libro por su ID
+    const libro = response.data.find(libro => libro.id === parseInt(id));
+
+    if (!libro) {
+      console.error('Libro no encontrado');
+      return;
+    }
+
+    // Actualizar el libro
+    libro.titulo = nuevoTitulo;
+    libro.autor = nuevoAutor;
+
+    // Actualizar el libro en el servidor
+    await axios.put(`${apiUrl}/${id}`, { titulo: nuevoTitulo, autor: nuevoAutor });
+
+    console.log('Libro actualizado:', libro);
+  } catch (error) {
+    console.error('Error al actualizar el libro:', error);
+  }
+};
+
+// Ejemplos de uso
+crearReserva(1, 'John Doe');  //reservar el libro con ID 1
+actualizarLibro(1, 'Nuevo Título', 'Nuevo Autor');  // actualizar el libro con ID 1
+
